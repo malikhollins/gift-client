0f8ad2f894301430d12aacdf65b738b7702eac9e
Malachi H. <thesouthographic@gmail.com>
2024-11-10 15:01:48 -0500

    Adding comments to item rank desc

--- ServerApp/ServerApp/dbo/Tables/Lists.sql
+++ ServerApp/ServerApp/dbo/Tables/Lists.sql
@@ -6,7 +6,10 @@
 
     [Owner] INT NOT NULL,
 
-    [ItemRankDescription] VARCHAR(100) NOT NULL, 
+    [ItemRankDescription] VARCHAR(250) NOT NULL, 
+
+    -- The rank of all the items the user has formated to a string delimted ',' e.g. "listItemId 1,listItemId 2,...,listItemId X". 
+    -- The amount of items a user can have in a list is limited to a constant.
 
     [Name] VARCHAR(30) NULL,
 
