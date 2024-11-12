864ff01345aabbe2a781f7efb7adf88010a4ed05
Malachi H. <thesouthographic@gmail.com>
2024-11-11 21:26:30 -0500

    How to - blazor

---
+++ ClientApp/ClientApp/Models/GiftHouse.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientApp.Models
+{
+   public class GiftHouse
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "The Gifthouse name must be between 2 and 50 characters.")]
+        public string? HouseName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Your name/alias must be between 2 and 50 characters.")]
+        public string? CreatorName { get; set; }
+    }
+}
