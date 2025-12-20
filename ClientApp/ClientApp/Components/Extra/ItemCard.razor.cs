6223648046b96b3506d259c86202da5879b11662
Malik "Malachi" Hollins <thesouthographic@gmail.com>
2025-12-20 15:37:25 -0500

    Updating styles

---
+++ ClientApp/ClientApp/Components/Extra/ItemCard.razor.cs
@@ -0,0 +1,14 @@
+﻿using ClientApp.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace ClientApp.Components.Extra
+{
+    public partial class ItemCard
+    {
+        [Parameter]
+        public int ListId { get; set; }
+
+        [Parameter]
+        public Item Model { get; set; } = new();
+    }
+}
